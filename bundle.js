50e96613af81a38d84c45311e50e54604a365a83
Cold5tar <58589094+Cold5tar@users.noreply.github.com>
2020-05-16 19:30:25 +0300

    Update bundle.js

--- bundle.js
+++ bundle.js
@@ -72395,12 +72395,12 @@ module.exports = class Viewer {
 	this.state.colorC = 8;
 	this.state.genericC = 8;
 	const geometryFolder = gui.addFolder('Geometry');
-	compressionLevel = geometryFolder.add(this.state, 'compressionLevel', 0, 10, 1).min(0).listen();
-	positionC = geometryFolder.add(this.state, 'position', 0, 32, 1).min(0).listen();
-	texcoordC = geometryFolder.add(this.state, 'texcoord', 0, 32, 1).min(0).listen();
-	normalC = geometryFolder.add(this.state, 'normal', 0, 32, 1).min(0).listen();
-	colorC = geometryFolder.add(this.state, 'color', 0, 32, 1).min(0).listen();
-	genericC = geometryFolder.add(this.state, 'generic', 0, 32, 1).min(0).listen();
+	const compressionLevel = geometryFolder.add(this.state, 'compressionLevel', 0, 10, 1).min(0).listen();
+	const positionC = geometryFolder.add(this.state, 'position', 0, 32, 1).min(0).listen();
+	const texcoordC = geometryFolder.add(this.state, 'texcoord', 0, 32, 1).min(0).listen();
+	const normalC = geometryFolder.add(this.state, 'normal', 0, 32, 1).min(0).listen();
+	const colorC = geometryFolder.add(this.state, 'color', 0, 32, 1).min(0).listen();
+	const genericC = geometryFolder.add(this.state, 'generic', 0, 32, 1).min(0).listen();
 
 
 
